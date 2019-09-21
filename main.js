a035f6c12d1c5ca06a8ab087b274e925b404e8f4
aba20 <55111699+aba20@users.noreply.github.com>
2019-09-21 23:14:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [cardOne.push(console.log("User flipped king"), cardTwo.push(console.log("User flipped queen")];
+
+var cardOne = cards[0];
+
+var cardTwo = cards[2];
+
+if (cardsInPlay.length === 2){
+
+}
+else if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+}
+else (cardsInPlay[0] !== cardsInPlay[1]){
+    alert("Sorry try again.");
+}
