ca0ab11e4e37c7fd85d91977f8f012f8ef4a593c
aba20 <55111699+aba20@users.noreply.github.com>
2019-09-28 23:32:46 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cardOne.push(console.log("User flipped king")), cardTwo.push(console.log("User flipped queen"))];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
 
+    }
+];
 
-function chackForMach(){
-    if (cardsInPlay[0] === cardsInPlay[2]) {
-        console.log("You found a match!");
-}
-    else {
-        console.log("Sorry, try again.");
-}
-};
+var cardsInPlay = [cardOne.push(console.log("User flipped king"), cardsInPlay.push(cards[cardId].rank)), cardTwo.push(console.log("User flipped queen"), cardsInPlay.push(cards[cardId].rank)];
 
-if (cardsInPlay.length === 2) {};
-
-for (cardsInPlay[0] === cardsInPlay[1]):{
-    alert("You found a match!");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
 };
 
 function flipCard(cardId) {
-    console.log("User flipped" + card[cardId]);
+    chackForMatch()
+    console.log("User flipped" + card[cardId].rank);
 };
 
 flipCard([0]);
 flipCard([2]);
+
+console.log(cardImage);
+console.log(suit)
