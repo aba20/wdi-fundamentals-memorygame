da2c8156ec4fd29c977d4c5c14cec9a479925e24
aba20 <55111699+aba20@users.noreply.github.com>
2019-09-28 22:56:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cardOne.push(console.log("User flipped king"), cardTwo.push(console.log("User flipped queen")];
+var cardsInPlay = [cardOne.push(console.log("User flipped king")), cardTwo.push(console.log("User flipped queen"))];
 
-var cardOne = cards[0];
 
-var cardTwo = cards[2];
-
-if (cardsInPlay.length === 2){
-
+function chackForMach(){
+    if (cardsInPlay[0] === cardsInPlay[2]) {
+        console.log("You found a match!");
 }
-else if (cardsInPlay[0] === cardsInPlay[1]){
+    else {
+        console.log("Sorry, try again.");
+}
+};
+
+if (cardsInPlay.length === 2) {};
+
+for (cardsInPlay[0] === cardsInPlay[1]):{
     alert("You found a match!");
-}
-else (cardsInPlay[0] !== cardsInPlay[1]){
-    alert("Sorry try again.");
-}
+};
+
+function flipCard(cardId) {
+    console.log("User flipped" + card[cardId]);
+};
+
+flipCard([0]);
+flipCard([2]);
